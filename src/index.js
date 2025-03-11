cd2c77d767167504b80f5ba48a563029e0462e36
damiands2727 <damianstella88@gmail.com>
2025-03-11 19:35:52 -0300

    removed unnecesary folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import "./services/serviceHelper";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import "toastr/build/toastr.min.css";
